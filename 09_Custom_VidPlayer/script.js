383f79ade7d9dc42c945280fde8c6c336323d6e9
Larry8900 <110744000+Larry8900@users.noreply.github.com>
2025-08-24 21:21:42 +0100

    Add files via upload

---
+++ 09_Custom_VidPlayer/script.js
@@ -0,0 +1,65 @@
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullscreenButton = player.querySelector('.fullscreen');
+
+/* Build out functions */
+
+const togglePlay = () => video.paused? video.play(): video.pause() ;
+
+function updateButton(){
+    const icon = video.paused? '►' : '❚ ❚';
+    toggle.textContent = icon ;
+}
+
+function skip(){
+    video.currentTime += parseInt(this.dataset.skip);
+}
+
+function toggleScreen() {
+    if (!document.fullscreenElement) {
+    player.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+
+
+/* Hook up the event listeners */
+
+video.addEventListener('click', togglePlay)
+video.addEventListener('click', updateButton)
+video.addEventListener('timeupdate', handleProgress)
+toggle.addEventListener('click', togglePlay)
+toggle.addEventListener('click', updateButton)
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+fullscreenButton.addEventListener('click', toggleScreen);
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousedown', ()=> mousedown = true);
+progress.addEventListener('mouseup', ()=> mousedown = false);
